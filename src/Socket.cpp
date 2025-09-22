6ef1312b05df8302b41cda3658d7bc2f516ac965
KONTROL Tecnologia <guzzy@uol.com.br>
2025-09-22 08:13:14 -0300

    Use modern OpenSSL certificate getters

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -492,7 +492,11 @@ void Socket::cleanSsl() {  // called when failure in ssl set up functions and fr
 long Socket::checkCertValid(String &hostname)
 {
     //check we have a certificate
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    X509 *peerCert = SSL_get1_peer_certificate(ssl);
+#else
     X509 *peerCert = SSL_get_peer_certificate(ssl);
+#endif
     if (peerCert == NULL) {
         return -1;
     }
@@ -513,10 +517,13 @@ X509_VERIFY_PARAM_free(param);
 //check the common name and altnames of a certificate against hostname
 int Socket::checkCertHostname(const std::string &_hostname)
 {
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
     String hostname = _hostname;
 
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    X509 *peercertificate = SSL_get1_peer_certificate(ssl);
+#else
     X509 *peercertificate = SSL_get_peer_certificate(ssl);
+#endif
     if (peercertificate == NULL) {
 #ifdef NETDEBUG
         std::cout << thread_id << "unable to get certificate for " << hostname << std::endl;
@@ -665,9 +672,7 @@ int Socket::checkCertHostname(const std::string &_hostname)
         X509_free(peercertificate);
         return 0;
     }
-#else  // is openssl v1.1 or above
-    return 0;    //TODO
-#endif
+    X509_free(peercertificate);
     return -1;
 }
 
